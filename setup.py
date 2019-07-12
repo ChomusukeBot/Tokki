c03c31d0b791d82fbb197295f8a831d2ba830df0
Hannele Ruiz <justlemoncl@gmail.com>
2019-07-12 17:43:00 -0400

    Added requirements on the Python installer

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 import setuptools
 
+# Open the requirements file and dump them
+with open("requirements.txt") as file:
+    requirements = file.read().splitlines()
+# Do the same with the dev requirements
+with open("requirements-dev.txt") as file:
+    extras = {"dev": file.read().splitlines()}
+
 
 setuptools.setup(
     name="tokki",
@@ -9,6 +16,8 @@ setuptools.setup(
     description="Set of Asynchronous APIs for various services.",
     url="https://github.com/ChomusukeBot/Tokki",
     packages=["tokki"],
+    install_requires=requirements,
+    extras_require=extras,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
