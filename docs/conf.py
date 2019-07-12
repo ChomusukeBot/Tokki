50e1ce608881d7ba9fd9b473eeed6111ccbf4e98
Hannele Ruiz <justlemoncl@gmail.com>
2019-07-12 03:53:19 -0400

    Added basics for the Sphinx docs

---
+++ docs/conf.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+# This is required for finding the Tokki package
+sys.path.insert(0, os.path.abspath("../"))
+
+# Basic information
+project = "Tokki"
+copyright = "2019 Hannele Ruiz"
+author = "Hannele Ruiz"
+
+
+# The extensions that are going to be loaded
+extensions = [
+]
+
+# Stuff to ignore when we are looking for documentation
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+# Files to be included in the output
+html_static_path = ["_static"]
+
+
+# The Theme to use for the HTTP content
+# This is required for Read the Docs
+html_theme = "mxtheme"
+# The custom theme options
+html_theme_options = {
+    "primary_color": "pink",
+    "accent_color": "cyan"
+}
