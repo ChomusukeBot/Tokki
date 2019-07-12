293f25d87a494242364dcb50c13d04325114c3b4
Hannele Ruiz <justlemoncl@gmail.com>
2019-07-12 18:04:41 -0400

    Added support for external Sphinx docs via the InterSphinx extension

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,8 @@ author = "Hannele Ruiz"
 # The extensions that are going to be loaded
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon"
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx"
 ]
 
 # Stuff to ignore when we are looking for documentation
@@ -15,6 +16,12 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # Files to be included in the output
 html_static_path = ["_static"]
 
+# Mapping of external docs that we need
+# In this case, the base Python docs and aiohttp
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "aiohttp": ("https://aiohttp.readthedocs.io/en/stable/", None)
+}
 
 # The Theme to use for the HTTP content
 # This is required for Read the Docs
