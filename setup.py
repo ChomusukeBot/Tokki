81a4dddb5d2de0a993422e10c5e4eb683ff34305
Hannele Ruiz <justlemoncl@gmail.com>
2019-07-12 05:14:42 -0400

    Added setup file for installing the package

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+setuptools.setup(
+    name="tokki",
+    version="0.1",
+    author="Hannele Ruiz",
+    author_email="lemon@justalemon.ml",
+    description="Set of Asynchronous APIs for various services.",
+    url="https://github.com/ChomusukeBot/Tokki",
+    packages=["tokki"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
